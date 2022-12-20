0ac06bac4567450c00a0eba319bd51e2a737aa51
SEKANLIN Principe <psekanlin11@gmail.com>
2022-12-20 19:50:04 +0000

    Add files via upload

---
+++ demo.ps1
@@ -0,0 +1,20 @@
+﻿# Connaitre un nombre de users dans une unité d'organisation spécifique et ses sous-unités d'organisation
+#######################################################
+# PREMIER SIMPLE SIMPLE                             #
+#######################################################
+#(Get-ADUser -Filter * -SearchBase “OU=Direction,OU=Personnel,DC=sprincipe,DC=sn”).count
+
+# Connaitre un nombre de users dans une unité d'organisation spécifique et ses sous-unités d'organisation
+#######################################################################################
+# DEUXIEME SCRIPT                                                                     #
+#Qui me permettra: le contenu de chaque unité d'organisation suivi de ses utilisateurs#
+#######################################################################################
+
+
+$ous = Get-ADOrganizationalUnit -Filter * -SearchBase "OU=Direction,OU=Personnel,DC=sprincipe,DC=sn" | Select-Object -ExpandProperty DistinguishedName
+$ous | ForEach-Object{
+    [psobject][ordered]@{
+        OU = $_
+        Count = (Get-ADUser -Filter * -SearchBase "$_").count
+    }
+}
